d9d0819662cee982f15ff799292141a90dd60d6e
Aashi <45652647+aashi0707@users.noreply.github.com>
2020-07-22 18:04:16 +0530

    dart-basic

---
+++ sum1.dart
@@ -0,0 +1,10 @@
+main() {
+  var p = lw(3, 5);
+
+  print(p); // null
+}
+
+lw(i, j) {
+  var z = i + j;
+  print(z); // 8
+}
---
+++ sum2.dart
@@ -0,0 +1,9 @@
+main() {
+  var p = lw(3, 5);
+  print(p);
+}
+
+lw(i, j) {
+  var z = i + j;
+  return (z);
+}
